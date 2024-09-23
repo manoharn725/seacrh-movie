e111bc7918b90ca10d3ee76668f1a196d1c32fa6
manoharn725 <manoharnmanu725@gmail.com>
2024-09-23 17:12:03 +0530

    changes

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./index.scss";
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const handleOnChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit(term);
+  };
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input type="text" value={term} onChange={handleOnChange} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default SearchBar;
