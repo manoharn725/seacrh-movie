cd067e431d60356da861bf7f5a87c9493f8fbe62
manoharn725 <manoharnmanu725@gmail.com>
2024-09-25 10:29:18 +0530

    added no-poster-available image

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,9 +1,10 @@
+import noPosterAvailabel from '../../assets/no-poster-available.png'
 import "./index.scss";
 
 const MovieCard = ({ movie }) => {
   return (
     <div className="card">
-        <img src={movie?.i?.imageUrl} />
+        <img src={movie?.i?.imageUrl || noPosterAvailabel} alt="no poster available" />
       <p>{movie?.l}</p>
     </div>
   );
