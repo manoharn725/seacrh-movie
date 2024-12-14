c67d465f7dac16b8653b9732597afefb98390357
manoharn725 <manoharnmanu725@gmail.com>
2024-12-14 21:29:53 +0530

    Refactor conditional rendering for empty movie list check

--- src/components/MoviesList/index.jsx
+++ src/components/MoviesList/index.jsx
@@ -2,14 +2,20 @@ import MovieCrad from "../MovieCard";
 import "./index.scss";
 
 const MoviesList = ({ moviesList, isLoading, isError }) => {
-  if (!Array.isArray(moviesList)) {
-    return <div>No movies found.</div>; 
-  }
-   return (
+  // if (!Array.isArray(moviesList)) {
+  //   return <div>No movies found.</div>;
+  // }
+  return (
     <div className="movies-list">
-      {moviesList.map((movie) => {
-        return <MovieCrad key={movie.id} movie={movie} isLoading={isLoading} />;
-      })}
+      {!Array.isArray(moviesList) ? (
+        <div>No movies found....</div>
+      ) : (
+        moviesList.map((movie) => {
+          return (
+            <MovieCrad key={movie.id} movie={movie} isLoading={isLoading} />
+          );
+        })
+      )}
     </div>
   );
 };
